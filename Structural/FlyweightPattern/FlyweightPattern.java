11cad2bc1e95f23b6eeb4b3c7c9d3f4a6977b3d6
DINESH-SANKAR-SELVI <dinesh.sankar0110@gmail.com>
2023-10-25 21:44:54 +0530

    Flyweight Source

--- Structural/FlyweightPattern/FlyweightPattern.java
+++ Structural/FlyweightPattern/FlyweightPattern.java
@@ -1,3 +1,8 @@
+/*
+ *A Flyweight Pattern says that just "to reuse already existing
+ *similar kind of objects by storing them and create new object when no 
+ *matching object is found". 
+*/
 package Structural.FlyweightPattern;
 
 public class FlyweightPattern {
