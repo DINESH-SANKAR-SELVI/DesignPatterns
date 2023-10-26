2d6495fdee7550ea63bea5a25840d1c4536950e0
DINESH-SANKAR-SELVI <dinesh.sankar0110@gmail.com>
2023-10-26 11:53:34 +0530

    FlyweightPattern Source

---
+++ CoreDesignPatterns/Structural/FlyweightPattern/FlyweightPattern.java
@@ -0,0 +1,10 @@
+/*
+ *A Flyweight Pattern says that just "to reuse already existing
+ *similar kind of objects by storing them and create new object when no 
+ *matching object is found". 
+*/
+package CoreDesignPatterns.Structural.FlyweightPattern;
+
+public class FlyweightPattern {
+    
+}
